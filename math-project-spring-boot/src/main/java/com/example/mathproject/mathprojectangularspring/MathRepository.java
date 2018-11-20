e9e4284a6bddaecca71ab711fca8186bf6f533eb
Shreya-Srinivasa <coolshreyasrinivasa@gmail.com>
2018-11-20 15:48:26 +0530

    Add files via upload

---
+++ math-project-spring-boot/src/main/java/com/example/mathproject/mathprojectangularspring/MathRepository.java
@@ -0,0 +1,7 @@
+package com.example.mathproject.mathprojectangularspring;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MathRepository extends JpaRepository<MathModel, Integer>{
+
+}
